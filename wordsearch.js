c682ff9e71098aee8038834c1f9fc7db8a8cc097
William <mack.mindenhall@gmail.com>
2020-07-01 22:21:03 +0000

    Added functionality to search vertically. Code still needs refactoring now

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => {
+    return ls.join("");
+  });
+  let verticalJoin = [];
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
-
-module.exports = wordSearch
+  }
+  for (let i = 0; i < horizontalJoin.length; i++) {
+    for (let j = 0; j < horizontalJoin[i].length; j++) {
+      if (!verticalJoin[j]) {
+        verticalJoin.push([]);
+      }
+      verticalJoin[j][i] = horizontalJoin[i][j];
+    }
+  }
+  verticalJoin = verticalJoin.map((v) => {
+    return v.join("");
+  });
+  for (v of verticalJoin) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+module.exports = wordSearch;
