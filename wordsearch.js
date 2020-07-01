a18dea964c29b89c0118db5188974a3beb6c404d
William <mack.mindenhall@gmail.com>
2020-07-01 23:35:54 +0000

    Refactored code

--- wordsearch.js
+++ wordsearch.js
@@ -1,28 +1,44 @@
-const wordSearch = (letters, word) => {
-  const horizontalJoin = letters.map((ls) => {
-    return ls.join("");
-  });
+const switchRowColumns = (arrayToChange) => {
   let verticalJoin = [];
-  for (l of horizontalJoin) {
+  for (let i = 0; i < arrayToChange.length; i++) {
+    for (let j = 0; j < arrayToChange[i].length; j++) {
+      if (!verticalJoin[j]) {
+        verticalJoin.push([]);
+      }
+      verticalJoin[j][i] = arrayToChange[i][j];
+    }
+  }
+  return verticalJoin;
+};
+
+const checkMatchingWord = (arrayToSearch, word) => {
+  for (l of arrayToSearch) {
     if (l.includes(word)) {
       return true;
     }
   }
-  for (let i = 0; i < horizontalJoin.length; i++) {
-    for (let j = 0; j < horizontalJoin[i].length; j++) {
-      if (!verticalJoin[j]) {
-        verticalJoin.push([]);
-      }
-      verticalJoin[j][i] = horizontalJoin[i][j];
-    }
-  }
-  verticalJoin = verticalJoin.map((v) => {
-    return v.join("");
+  return false;
+};
+
+const reduceArrayTo1D = (letters) => {
+  return letters.map((ls) => {
+    return ls.join("");
   });
-  for (v of verticalJoin) {
-    if (v.includes(word)) {
-      return true;
-    }
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = reduceArrayTo1D(letters);
+
+  let verticalJoin = [];
+
+  verticalJoin = switchRowColumns(horizontalJoin);
+  verticalJoin = reduceArrayTo1D(verticalJoin);
+
+  if (
+    checkMatchingWord(horizontalJoin, word) ||
+    checkMatchingWord(verticalJoin, word)
+  ) {
+    return true;
   }
   return false;
 };
